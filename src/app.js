5ae14364a738a6a7c665b8589224606c40b40238
Felipe Bogo <felipe.bogo@jupiter.co.ao>
2020-04-09 02:58:04 +0100

    desafio node.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const idx = repositories.findIndex(rep => rep.id === id);
+
+  let ret;
+
+  if (idx < 0) {
+    ret = response.status(400).json({ error: `Índice ${id} não encontrado.` })
+  } else {
+    const { likes } = repositories[idx];
+    const rep = { id, title, url, techs, likes };
+
+    repositories[idx] = rep;
+
+    ret = response.json(rep);
+  }
+
+  return ret;
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const idx = repositories.findIndex(rep => rep.id === id);
+
+  let ret;
+
+  if (idx < 0) {
+    ret = response.status(400).json({ error: `Índice ${id} não encontrado.` })
+  } else {
+    repositories.splice(idx,1);
+    ret = response.status(204).send();
+  }
+
+  return ret;
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const idx = repositories.findIndex(rep => rep.id === id);
+
+  let ret;
+
+  if (idx < 0) {
+    ret = response.status(400).json({ error: `Índice ${id} não encontrado.` })
+  } else {
+    const rep = repositories[idx];
+    rep.likes++;
+    
+    repositories[idx] = rep;
+
+    ret = response.json(rep);
+  }
+
+  return ret;
 });
 
 module.exports = app;
